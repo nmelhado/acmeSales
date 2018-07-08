801a1d2a471d11bca4759e3aace71a8bfe358b3d
Nicholas Melhado <n.melhado@gmail.com>
2018-07-08 17:06:51 -0400

    Final solution with reduce, and my original for loop commented out

--- acmeSales.js
+++ acmeSales.js
@@ -1,34 +1,4 @@
-// function generateCustomerSalesMap(salesData, customerData) {
-//   let final = {};
-//   for($i = 0; $i < customerData.length; $i++) {
-//     let sales = 0;
-//     for($y = 0; $y < salesData.length; $y++) {
-//       if(salesData[$y]['customerId'] === customerData[$i]['id']) {
-//         sales += salesData[$y]['total'];
-//       }
-//       final[customerData[$i]['name']] = sales;
-//     }
-//   }
-//   return final;
-// }
-
-// function generateCustomerSalesMap(salesData, customerData) {
-//   const figures = salesData.reduce(function(accumulated, obj) {
-//       let key = obj['customerId'];
-//       if(!accumulated[key]) {
-//         accumulated[key] = 0;
-//       }
-//       accumulated[key] += obj['total'];
-//       return accumulated;
-//     }, {});
-//   let final = {};
-//   for($i = 0; $i < customerData.length; $i++) {
-//     final[customerData[$i]['name']] = figures[customerData[$i]['id']];
-//   }
-//   return final;
-// }
-
-function generateCustomerSalesMap(salesData, customerData) {
+function generateCustomerSalesMap(salesData, customerData) { //reduce solution
   const customers = customerData.reduce(function(accumulated, obj) {
       let key = obj['id'];
       accumulated[key] = obj['name'];
@@ -43,5 +13,19 @@ function generateCustomerSalesMap(salesData, customerData) {
     return accumulated;
   }, {});
 }
+
+// function generateCustomerSalesMap(salesData, customerData) { //for loop solution
+//   let final = {};
+//   for($i = 0; $i < customerData.length; $i++) {
+//     let sales = 0;
+//     for($y = 0; $y < salesData.length; $y++) {
+//       if(salesData[$y]['customerId'] === customerData[$i]['id']) {
+//         sales += salesData[$y]['total'];
+//       }
+//       final[customerData[$i]['name']] = sales;
+//     }
+//   }
+//   return final;
+// }
 
 module.exports = generateCustomerSalesMap;
